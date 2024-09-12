2657a678e5e6de5b273bee7f660f6e3042cf36fa
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-09-12 09:29:58 +0200

    fix: link update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -85,7 +85,7 @@
         <NavLi class="lg:px-2 lg:mb-0" href="https://github.com/shinokada/svelte-teenyicons"
           >GitHub</NavLi
         >
-        <NavLi href="https://svelte-svg-icons.vercel.app/">Icon sets</NavLi>
+        <NavLi href="https://svelte-svg-icons.codewithshin.com/">Icon sets</NavLi>
       </NavUl>
       <div class="flex items-center ml-auto">
         <DarkMode class="inline-block dark:hover:text-white hover:text-gray-900" />
@@ -113,7 +113,7 @@
         <FooterLink class="lg:px-2 lg:mb-0" href="https://github.com/shinokada/svelte-teenyicons"
           >GitHub</FooterLink
         >
-        <FooterLink href="https://svelte-svg-icons.vercel.app/">Icon sets</FooterLink>
+        <FooterLink href="https://svelte-svg-icons.codewithshin.com/">Icon sets</FooterLink>
       </FooterLinkGroup>
     </div>
   </Footer>
